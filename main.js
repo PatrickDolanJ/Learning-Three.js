b89082bac392c6bb594db494f0cefe9da3e8bd76
Mama_Dutchy <datpoland@gmail.com>
2022-09-06 14:06:52 -0400

    resolution now scales properly, imported glft scene and works and all P COOL

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
+
 import * as THREE from './node_modules/three/build/three.module.js';
+import {GLTFLoader} from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 // Setting up scene, canvas, and renderer
 const scene = new THREE.Scene(); 
-const camera = new THREE.PerspectiveCamera( 75,window.innerWidth / window.innerHeight, 0.1, 1000 ); 
-const canvas = document.querySelector('#c')
+const canvas = document.querySelector('#c');
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
 const renderer = new THREE.WebGLRenderer({canvas},true);
+//renderer.setPixelRatio( canvas.devicePixelRatio );
 
+const glLoader = new GLTFLoader();
+glLoader.load('./resources/maple_tree/scene.gltf',(gltf)=>{
+  const tree = gltf.scene;
+  tree.position.set(0,0,-5);
+  tree.scale.set(.05,.05,.05);
+  scene.add(tree);
+});
+
+
+//Making Scene grid Helper
+const axes = new THREE.GridHelper(10,10);
+scene.add(axes);
+//Making SkySphere
+const skySphere = new THREE.SphereGeometry(100,10,7); 
+const skyMaterial = new THREE.MeshBasicMaterial({color: 0x76c9db, side:THREE.BackSide});
+const skyBoxMesh = new THREE.Mesh(skySphere,skyMaterial);
+scene.add(skyBoxMesh);
 //making the cube
 const geometry = new THREE.BoxGeometry();
 const cubegeometry = new THREE.BoxGeometry();
+const pointsGeometry = new THREE.SphereGeometry(1,10,8);
 const material = new THREE.MeshPhongMaterial({ color: 0xd87b94 });
 const cube = new THREE.Mesh(cubegeometry, material);
-cube.position.set(5,0,0);
+cube.position.set(-3,0,0);
 const wirecube = new THREE.EdgesGeometry(geometry);
 const line = new THREE.LineSegments( wirecube, new THREE.LineBasicMaterial( { color: 0xffffff } ) );
-scene.add(line);
+const points_material = new THREE.PointsMaterial({
+  color: 'pink',
+  size: 0.1
+});
+
+const points_sphere = new THREE.Points(pointsGeometry, points_material);
+points_sphere.position.set(3,0,0);
+scene.add(points_sphere);
 scene.add(cube);
 camera.position.z = 5;
 
@@ -26,11 +54,25 @@ camera.position.z = 5;
     scene.add(light);
   }
 
+function render(time){
+  if(resizeRendererToDisplaySize(renderer)){
+    time *= 0.001;
+  const canvasDom = renderer.domElement;
+  camera.aspect = canvasDom.clientWidth /canvasDom.clientHeight;
+  camera.updateProjectionMatrix();
+  }
+}
 
-
-
-
-
+function resizeRendererToDisplaySize(renderer) {
+  const canvasDom = renderer.domElement;
+  const width = canvasDom.clientWidth;
+  const height = canvasDom.clientHeight;
+  const needResize = canvasDom.width !== width || canvasDom.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
 
 const animate = function () {
     requestAnimationFrame(animate);
@@ -40,23 +82,12 @@ const animate = function () {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 
+    points_sphere.rotation.x += 0.01;
+    points_sphere.rotation.y += 0.01;
+
+    camera.translateY(0.01);
+    render();
     renderer.render(scene, camera);
     };
     animate();
-
-    // function resizeCanvasToDisplaySize() {
-    //     const canvas = renderer.domElement;
-    //     // look up the size the canvas is being displayed
-    //     const width = canvas.clientWidth;
-    //     const height = canvas.clientHeight;
-      
-    //     // you must pass false here or three.js sadly fights the browser
-    //     renderer.setSize(width, height, false);
-    //     camera.aspect = width / height;
-    //     camera.updateProjectionMatrix();
-      
-    //     // update any render target sizes here
-    //   }
-      
-    //   const resizeObserver = new ResizeObserver(resizeCanvasToDisplaySize);
-    //   resizeObserver.observe(canvas, {box: 'content-box'});
+    
